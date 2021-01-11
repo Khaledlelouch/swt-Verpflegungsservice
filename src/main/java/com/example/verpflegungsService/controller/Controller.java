4e0dc9ec4b6a95d22a74dbc4eb300dbe4c2fee8a
majdi-91 <123majdi321@gmail.com>
2021-01-11 15:12:40 +0100

    Majdi: Sekretaer und Patient, noch nicht vollständig

---
+++ src/main/java/com/example/verpflegungsService/controller/Controller.java
@@ -0,0 +1,38 @@
+package com.example.verpflegungsService.controller;
+
+import com.example.verpflegungsService.model.Essen;
+import com.example.verpflegungsService.model.Mahlzeitstyp;
+import com.example.verpflegungsService.model.Patient;
+import com.example.verpflegungsService.model.Station;
+
+import java.util.List;
+
+public class Controller {
+
+	public Patient getPatient(String name) {
+		return null;
+	}
+
+	/**
+	 *  
+	 */
+	public List<Essen> getEssen(Mahlzeitstyp typ) {
+		return null;
+	}
+
+	public List<Station> getStationsListe() {
+		return null;
+	}
+
+	/**
+	 *  
+	 */
+	public void anmelden(String name, String passwort) {
+
+	}
+
+	public void abmelden() {
+
+	}
+
+}
